2a53ad0c4cb2e2a45b4598ba4d95edc55c38d9c2
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2020-04-05 16:54:19 +0200

    Update author name

--- Detect.pm
+++ Detect.pm
@@ -159,13 +159,13 @@ L<https://github.com/michal-josef-spacek/Dicom-File-Detect>
 
 =head1 AUTHOR
 
-Michal Špaček L<mailto:skim@cpan.org>
+Michal Josef Špaček L<mailto:skim@cpan.org>
 
 L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
 
- © 2014-2015 Michal Špaček
+ © 2014-2015 Michal Josef Špaček
  BSD 2-Clause License
 
 =head1 VERSION
